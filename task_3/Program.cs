6430a9fe2f6cd800f6d763b8741b67f7c48a15d7
MariaMagdalina <metodist_rmmc@mail.ru>
2022-08-23 02:23:06 +0300

    task 3

---
+++ task_3/Program.cs
@@ -0,0 +1,7 @@
+﻿// Данная программа получает на вход цифру, соответствующую дню недели, и выводит сообщение  - выходной это или нет. 
+Console.Write("Введите цифру от 1 до 7 ");
+int dayofweek = Convert.ToInt32(Console.ReadLine());
+if(dayofweek == 6 || dayofweek == 7)
+   Console.WriteLine("Это выходной!!!");
+else 
+   Console.WriteLine("Это не выходной :(");
