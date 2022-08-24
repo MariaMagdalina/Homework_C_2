1ad03e5831df79050cb5d9310c7e1e51b9954185
MariaMagdalina <metodist_rmmc@mail.ru>
2022-08-24 21:56:13 +0300

    Новое решение задачи

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -1,26 +1,27 @@
-﻿// Данная программа выводит третью цифру случайно сгенерированного числа от 1 до 65536
-int randomnum = new Random().Next(1, 65537);
+﻿// Данная программа выводит третью цифру случайно сгенерированного числа 
+long randomnum = new Random().Next(1, 1000000000);
 Console.WriteLine("Случайно сгенерированное число: " + randomnum);
-int thirddigit; // переменная для записи третьей цифры
-if (randomnum / 10000 > 0)
-{
-   thirddigit = (randomnum % 1000) / 100;
+
+// определение количества разрядов в числе
+int i = 10; // переменная для подсчета разрядности числа, присвоили значение 10 для избежания лишнего действия в цикле.  
+bool t = false;
+while(t == false)
+{ 
+   if(randomnum/i > 0)
+      i = i*10;  
+   else 
+      t = true;    
 }
+// в результате значение разрядности получается в 10 раз больше, учтем это в дальнейшем в формуле получения третьей цифры  
+if(i <= 100)
+   Console.WriteLine("Третьей цифры в числе нет");
+else if(i >= 10000)
+  {
+    long digit_third = (randomnum / (i / 1000)) % 10;
+    Console.WriteLine("Третья цифра в числе: " + digit_third); 
+  }
 else
- if (randomnum / 1000 > 0)
-{
-   thirddigit = (randomnum % 100) / 10;
+{ 
+    long digit_third = randomnum % 10;   
+    Console.WriteLine("Третья цифра в числе: " + digit_third);  
 }
-else
-  if (randomnum / 100 > 0)
-{
-   thirddigit = randomnum % 10;
-}
-else 
-   thirddigit = -1;
-//Вывод результата   
-if (thirddigit == -1) 
-   Console.WriteLine("Третьей цифры нет");
-else 
-   Console.WriteLine("Третья цифра: " + thirddigit);
-   
